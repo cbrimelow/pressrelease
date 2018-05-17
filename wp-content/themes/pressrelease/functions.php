40d230f18e8267b13f1d5264bf24494bb2b65921
Christopher Brimelow <cbrimelow@crf.org>
2018-05-17 16:10:30 -0400

    Added subnav dropdown for Late Breaking Trials, edited functions.php to redirect user to their referrer section after login and logout, made toolbar viewable to admins but not subscribers.

--- wp-content/themes/pressrelease/functions.php
+++ wp-content/themes/pressrelease/functions.php
@@ -1,16 +1,26 @@
 <?php
 
-show_admin_bar(false);
+if ( ! current_user_can( 'manage_options' ) ) {
+    show_admin_bar( false );
+}
 
 function auto_redirect_external_after_logout(){
-  wp_redirect( '/' );
-  exit();
+    if ( false !== strpos( $_SERVER['HTTP_REFERER'], 'wp-admin' ) ){
+        wp_redirect( '/admin' );
+    } else {
+        wp_redirect( '/' );
+    }
+    exit;
 }
 add_action( 'wp_logout', 'auto_redirect_external_after_logout');
 
 function auto_redirect_external_after_login(){
-  wp_redirect( '/' );
-  exit();
+    if ( false !== strpos( $_SERVER['HTTP_REFERER'], 'wp-login' ) ){
+        wp_redirect( '/admin' );
+    } else {
+        wp_redirect( '/' );
+    }
+  	exit();
 }
 add_action( 'wp_login', 'auto_redirect_external_after_login');
 
@@ -21,5 +31,21 @@ function member_check_shortcode( $atts, $content = null ) {
 		return $content;
 	return '';
 }
+// Disable wpautop filter that adds empty <p> for line breaks
 remove_filter( 'the_content', 'wpautop' );
 remove_filter( 'the_excerpt', 'wpautop' );
+
+/**
+ * Function for redirecting users on login based on user role
+
+function my_login_redirect( $url, $request, $user ){
+    if( $user && is_object( $user ) && is_a( $user, 'WP_User' ) ) {
+        if( $user->has_cap( 'administrator' ) ) {
+            $url = admin_url();
+        } else {
+            $url = home_url('/members-only/');
+        }
+    }
+    return $url;
+}
+ */
